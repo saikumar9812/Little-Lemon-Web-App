967d487f3d1228bb989f8b2d36e1b982578b9e3a
saikumar9812 <138357072+saikumar9812@users.noreply.github.com>
2025-01-25 20:02:49 +0530

    Update urls.py

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,12 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
-
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("book/", views.book, name="book"),
-    # Add the remaining URL path configurations here
     path("menu/", views.menu, name="menu"),
     path("menu_item/<int:pk>/", views.display_menu_items, name="menu_item"),
+    
+    # Add the API token authentication endpoint
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # This will enable the token authentication route
+    
+    # Optionally, you can add more views or endpoints as needed
 ]
